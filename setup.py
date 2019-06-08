4fa860b373508b4d6a1379b8ddfed0ad267abf2a
Ziba Gandomkar <37933707+ZibaGandomkar@users.noreply.github.com>
2019-06-08 17:50:12 +1000

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AnnotateMammo',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='zibag',
+    author_email='',
+    description=''
+)
